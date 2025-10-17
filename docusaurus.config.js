0117f42db0063402e3552c4a46bc5a7518c01cd9
C1XTZ <C1XTZ@proton.me>
2025-10-18 00:28:23 +0200

    chore: docusaurus 3.8.1 → 3.9.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,11 @@ const config = {
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
